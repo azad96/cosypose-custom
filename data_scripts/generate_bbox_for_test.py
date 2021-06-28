e36a93333952e186c93ae8eab5fe8da35c4756a2
none <none>
2021-06-28 20:36:31 +0300

    scripts are added for pickle and test.json files

---
+++ data_scripts/generate_bbox_for_test.py
@@ -0,0 +1,61 @@
+import os
+from os.path import join as osp
+import json
+import numpy as np
+import pickle
+
+pth = "/mnt-ssd/datasets/BM/kuatless/test-collision_pbr_1080_810"
+# pth = "/raid/datasets/Bright_Machines/Bin_Picking/kuatless/test_pbr_1080_810"
+# pth = "/mnt/trains/users/azad/BM/docker/local_data/bop_datasets/kuatless/test_pbr_1080_810"
+# save_pth = "/mnt/trains/users/azad/BM/docker/local_data/saved_detections/kuatless_test_1080_810.pkl"
+save_pth = "/mnt/trains/users/azad/BM/cosypose_azd/local_data/saved_detections/kuatless_test-collision-visib_1080_810.pkl"
+# save_pth = "/mnt-ssd/datasets/BM/bm2/bm2_test3_1080_810.pkl"
+scene_ids = sorted(os.listdir(pth))
+img_id = 0
+
+out_dict = dict()
+for scene_id in scene_ids:
+    scene_gt_info_pth = osp(pth, scene_id, "scene_gt_info.json")
+    scene_gt_pth = osp(pth, scene_id, "scene_gt.json")
+
+    f = open(scene_gt_info_pth ,"r")
+    scene_gt_info = json.load(f)
+    f.close() 
+    f = open(scene_gt_pth ,"r")
+    scene_gt = json.load(f)
+    f.close() 
+
+    for view_id in scene_gt_info.keys():
+        out_dict["{:04}/{:06}".format(int(scene_id), int(view_id))] = dict()
+        current_scene_gt_info = scene_gt_info["{}".format(int(view_id))]
+        bboxes = []
+        scores = []
+        poses = []
+        labels = []
+        dummy_pose = np.array([[1.0, 0., 0., 0.], [0., 1.0, 0., 0.], [0., 0., 1.0, 0.], [0., 0., 0., 1.0]])
+        for i ,obj in enumerate(current_scene_gt_info):
+            current_gt = scene_gt[view_id][i]
+            current_obj_id = current_gt['obj_id']
+
+            # current_bbox = obj["bbox_obj"]
+            current_bbox = obj["bbox_visib"]
+            x,y,w,h = current_bbox
+            if x == -1 and y == -1 and w == -1 and h == -1:
+                continue
+            # if x < 0 or y < 0 or w < 0 or h < 0:
+            #     continue
+            #bboxes.append([x, y, x + w, y + h])
+            bboxes.append([y, x, y + h, x + w])
+            scores.append(1.0)
+            poses.append(dummy_pose)
+            labels.append("obj_{:06}".format(current_obj_id))
+        scores = np.array(scores)
+        bboxes = np.array(bboxes)
+        out_dict["{:04}/{:06}".format(int(scene_id), int(view_id))]['scores'] = scores
+        out_dict["{:04}/{:06}".format(int(scene_id), int(view_id))]['labels_txt'] = labels
+        out_dict["{:04}/{:06}".format(int(scene_id), int(view_id))]['poses'] = poses
+        out_dict["{:04}/{:06}".format(int(scene_id), int(view_id))]['rois'] = bboxes
+
+f = open(save_pth, "wb")
+pickle.dump(out_dict, f)
+
---
+++ data_scripts/generate_target_json.py
@@ -0,0 +1,50 @@
+import os
+from os.path import join as osp
+import json
+
+
+
+pth = "/mnt-ssd/datasets/BM/kuatless/test-collision_pbr_1080_810"
+save_pth = "/mnt-ssd/datasets/BM/kuatless/kuatless_test-collision_1080_810.json"
+# pth = "/raid/datasets/Bright_Machines/Bin_Picking/kuatless/test_pbr_1080_810"
+# save_pth = "/raid/datasets/Bright_Machines/Bin_Picking/kuatless/kuatless_test_1080_810.json"
+# pth = "/mnt/trains/users/azad/BM/docker/local_data/bop_datasets/kuatless/test_pbr_1080_810"
+# save_pth = "/mnt/trains/users/azad/BM/docker/local_data/bop_datasets/kuatless/kuatless_test_1080_810.json"
+scene_ids = sorted(os.listdir(pth))
+img_id = 0
+
+out_dict = dict()
+save_list = []
+total_entry = 0
+for scene_id in scene_ids:
+    out_dict["{}".format(int(scene_id))] = dict()
+
+    scene_gt_pth = osp(pth, scene_id, "scene_gt.json")
+    f = open(scene_gt_pth ,"r")
+    scene_gt = json.load(f)
+    f.close() 
+    
+    for view_id in scene_gt.keys(): #[0, 8, 12, 16]:#scene_gt.keys():
+        current_scene_gt = scene_gt["{}".format(int(view_id))]
+        out_dict["{}".format(int(scene_id))]["{}".format(int(view_id))] = dict()
+        for obj in current_scene_gt:
+            obj_id = str(obj["obj_id"])
+            if not obj_id in out_dict["{}".format(int(scene_id))]["{}".format(int(view_id))].keys():
+                out_dict["{}".format(int(scene_id))]["{}".format(int(view_id))]["{}".format(obj_id)] = 1
+                total_entry += 1
+            else:
+                out_dict["{}".format(int(scene_id))]["{}".format(int(view_id))]["{}".format(obj_id)] += 1
+        for k, v in out_dict["{}".format(int(scene_id))]["{}".format(int(view_id))].items():
+            save_list.append([int(scene_id), int(view_id), int(k), int(v)])
+target_list = [] 
+for i, obj in enumerate(save_list):
+    dummy = dict()
+    dummy["im_id"] = obj[1]
+    dummy["inst_count"] = obj[3]
+    dummy["obj_id"] = obj[2]
+    dummy["scene_id"] = obj[0]
+    target_list.append(dummy)
+
+f = open(save_pth, "w")
+json.dump(target_list, f)
+f.close()
